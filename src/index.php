c92f4c568f3636d1aaebc4753f7ef96f9c15b6a1
Andres <andresrafael.bv@gmail.com>
2024-07-11 15:06:29 -0400

    Commit 1

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "db";
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully to the database!<br>";
+echo "Hello, World!";
+?>
